86cc9aa87690284d701571b57b7501ce08e2cf69
Aleksandar Gujas <aleksandar.gujas@luxuryescapes.com>
2025-06-22 19:25:29 +1000

    :tada: init

---
+++ index.ts
@@ -0,0 +1,27 @@
+# On Comedy
+
+I have always loved comedy. As a child I would keenly await each year's televised Melbourne comedy festival and crouch, alert, with VCR at the ready, to tape the four hour comedy bonanza for the next 365 days' repeated (to my sister's nauseation) consumption. My first careen into the internet (in 2000) was a rabid search for jokes - fullofjokes.com (defunct), funnyjunk.com (fortunately or unfortunately, alive and well), and many more now lost to me.
+
+At the same time, I had no concept of _why_ I enjoyed comedy so much. In my teens I played around with various high-minded philosophical glazings of comedy as truth-to-power, vital satire, some high form of rhetoric and so on. Later, I thought of comedy perhaps as some kind of shortcut for new and enlightening concepts "snuck" to the audience by the greats - Carlin, Pryor and those guys. Anyway, this is the kind of thing heady teenagers with too many feelings think of.
+
+I find most of these concepts to be false. My experience with comedians is that they are no more enlightened than anyone else (probably I am the fool for ever thinking otherwise), and often terribly broken petty-narcissists and attention seekers. Satire, also, hardly seems to make any change in the world. My appraisal of its effect seems to be mainly in instilling a knowing feeling, sometimes knowing superiority, in the consumer, or more usefully as a historical record of some set of mores and views in a people, curmudgeonly or otherwise (we thank you, Juvenal). 
+
+And yet, to this day I have a fond love of comedy (though the grand thinkers of the craft have fallen away in my questionably valuable favour).
+
+## Comedy Doesn't Mean Anything (Thank God)
+
+They say (they being some scientists who I can't remember and who may be wrong) that laughter originally evolved in a function similar to tremouring in mammals - a surprise anticipates danger, if there is no danger laughter physiologically resets the body. 
+
+Jokes then, are the manufacturing of surprise and resolution in quick succession, minus the danger. They are merely a trick, a slight-of-voice, piggybacking on a tool in our evolutionary box to survive the Savannah.
+
+Am I just a laughter addict? I don't think that's everything. I opined to my wonderful wife that comedy has gotten me through many dark times, such that I hesitate to mark anything off-bounds for comedy (save the Divine, hold that thought). 
+
+So here it is - I have matured beyond high-minded philosophising on comedy. I shall now engage in low-minded philosophising on comedy. It's a personal sort of philosophising, but this entire essay is quite personal so I hope you can forgive a little more. 
+
+Comedy doesn't teach us anything - in fact, it's entire utility is in that it reduces. Laughing at something reminds us it is not God, that it is of our world and therefore of finite importance. This can be healing, this can be therapy. 
+
+Consider the origin of comedic theatre - the Satyr plays, always between the great tragedies of Aeschylus and co. They were a reminder - "hey don't feel too bad, this is all of the Earth, and the Earth ain't sticking around for any of us." Comedy pulls us all down to Earth, and that is its only lesson. Some of us only need to learn this about work on Mondays and our friends, others move the boundary of "jokeable" further out, and only the purest materialists will joke about God. 
+
+The Orphic Wheel, Apollonian striving, Christ. I think I understand why Christ does not joke much in the New Testament - it is not His domain. He sits above the things of the world, and perhaps left us jokes to pull down the remainder to their rightful location. 
+
+
